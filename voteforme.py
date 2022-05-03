326f537b94cac78890dff9a06fb9efe2ff5d13f2
Liamsimp69420 <100945226+Liamsimp69420@users.noreply.github.com>
2022-05-03 18:20:24 -0400

    Add files via upload

---
+++ voteforme.py
@@ -0,0 +1,10 @@
+from multiprocessing.connection import wait
+from webbrowser import open_new_tab
+
+
+vote = input('Who are you voting for? ') 
+if vote =='William' or vote =='william': open_new_tab('https://liamsimp69420.github.io/liamsimp69420/'), 
+elif vote =='Liam' or vote=='liam': open_new_tab('https://liamp.dev') 
+else: open_new_tab('https://www.youtube.com/watch?v=dQw4w9WgXcQ') 
+print('Thank you for voting for ' + vote) 
+
